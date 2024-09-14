c322d2600354c5617477d0c8818e43514a51c08e
user <user@LAPTOP-1PL3S4MD>
2024-09-14 19:35:31 +0800

    加入專案檔案。

---
+++ TeaTimeDemo/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TeaTimeDemo.Models;
+
+namespace TeaTimeDemo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
